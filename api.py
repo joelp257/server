1e1928b1c9f21170b557e3972ba814053d772bcc
joelp257 <joelvayalirakkathu@gmail.com>
2024-03-10 15:54:07 +0530

    version 2

--- api.py
+++ api.py
@@ -1,18 +1,16 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify,request
 import json
 import pandas as pd
 from pycaret.classification import *
-from flask_cors import CORS
+from pycaret.regression import *
 
 app = Flask(__name__)
 
 
-@app.route('/query_d', methods=['GET'])
-def process_query():
+@app.route('/diabetes', methods=['GET'])
+def process_diabetic_query():
     try:
-        #data = json_value()
         data = request.get_json()
-        preg = data.get('Pregnancies')
         glu = data.get('Glucose')
         blo = data.get('BloodPressure')
         ski = data.get('SkinThickness')
@@ -20,20 +18,44 @@ def process_query():
         bmi = data.get('BMI')
         dia = data.get('DiabetesPedigreeFunction')
         age = data.get('Age')
-        out = data.get('Outcome')
-        print(preg)
 
-        col_names = pd.read_pickle('./col_names.pkl')
+        col_names = pd.read_pickle('.\d_pred_col_names.pkl')
         print(col_names)
-        predictions=[preg,glu,blo,ski,ins,bmi,dia,age,out]
+        predictions=[glu,blo,ski,ins,bmi,dia,age,1]
         df=pd.DataFrame([predictions],columns=col_names)
-        model_loaded=load_model('./best_model')
+        model_loaded=load_model('.\d_pred.pickle')
         predicted = predict_model(model_loaded,data=df)
-        prediction_list = predicted['prediction_label'].tolist()
 
-        response_data = {           
+        response_data = {
             'result': 'success',
-            'prediction_label':prediction_list
+            'prediction_label': predicted['prediction_label']
+        }
+
+        return jsonify(response_data)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+@app.route('/cholestrol', methods=['GET'])
+def process_chol_query():
+    try:
+        data = request.get_json()
+        age = data.get('age')
+        sex = data.get('sex')
+        cp = data.get('cp')
+        trest = data.get('trestbps')
+        cho = data.get('chol')
+
+        col_names = pd.read_pickle('\chol_pred_col_names.pkl')
+        print(col_names)
+        predictions=[age,sex,cp,trest,0]
+        df=pd.DataFrame([predictions],columns=col_names)
+        model_loaded=load_model('\chol_pred.pickle')
+        predicted = predict_model(model_loaded,data=df)
+
+        response_data = {
+            'result': 'success',
+            'prediction_label': predicted['prediction_label']
         }
 
         return jsonify(response_data)
@@ -41,6 +63,35 @@ def process_query():
     except Exception as e:
         return jsonify({'error': str(e)})
 
+
+@app.route('/bp', methods=['GET'])
+def process_bp_query():
+    try:
+        data = request.get_json()
+        glu = data.get('Glucose')
+        blo = data.get('BloodPressure')
+        ski = data.get('SkinThickness')
+        ins = data.get('Insulin')
+        bmi = data.get('BMI')
+        dia = data.get('DiabetesPedigreeFunction')
+        age = data.get('Age')
+
+        col_names = pd.read_pickle('.\bp_pred_col_names.pkl')
+        print(col_names)
+        predictions=[glu,0,ski,ins,bmi,dia,age]
+        df=pd.DataFrame([predictions],columns=col_names)
+        model_loaded=load_model('.\bp_pred.pickle')
+        predicted = predict_model(model_loaded,data=df)
+
+        response_data = {
+            'result': 'success',
+            'prediction_label': predicted['prediction_label']
+        }
+
+        return jsonify(response_data)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000,debug=True)
---
+++ diabetes_prediction/diabetic_prediction.py
@@ -0,0 +1,37 @@
+
+
+import numpy as np
+import pandas as pd
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+
+data = pd.read_csv("diabetes.csv")
+
+# Explore the data
+print("Shape of the dataset:", data.shape)
+print("First 5 rows of the dataset:\n", data.head())
+
+X= data.iloc[:,:]
+
+# prompt: import the pycaret regresssion
+
+
+
+from pycaret.classification import *
+
+import pycaret
+clf = pycaret.classification.setup(data=X, target="Outcome")
+
+clf.compare_models()
+
+best_model = clf.compare_models()
+
+#evaluate_model(best_model)
+
+col_names_list=data.columns.tolist()
+pd.Series(col_names_list).to_pickle('d_pred_col_names.pkl')
+
+with open('d_pred.pickle', 'wb') as handle:
+    pickle.dump(best_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
